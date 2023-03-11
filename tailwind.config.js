4d9dba18c77e8e2c27d017145eac61aa8c3474ec
yeremialucky <luckytumiwang@gmail.com>
2023-03-11 08:09:44 +0800

    tugas dua

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#0066FE",
+        secondary: "#3252DF",
+        darker: "#152C5B",
+      },
+    },
+  },
+  plugins: [],
+};
